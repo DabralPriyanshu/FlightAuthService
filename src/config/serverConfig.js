75b47401bd896f2366133de048ec5981b74b1491
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-01-21 19:43:57 +0530

    Added jwt token creation and token validation logic with password comparison logic

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -2,4 +2,5 @@ require("dotenv").config({ quiet: true });
 
 module.exports = {
   PORT: process.env.PORT,
+  JWT_KEY: process.env.JWT_KEY,
 };
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,5 +1,7 @@
 const UserRepository = require("../repository/user-repository");
-
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { JWT_KEY } = require("../config/serverConfig");
 class UserService {
   constructor() {
     this.repository = new UserRepository();
@@ -17,6 +19,30 @@ class UserService {
       return this.repository.getById(userId);
     } catch (error) {
       console.log("Something went wrong in service layer");
+      throw error;
+    }
+  }
+  createToken(user) {
+    try {
+      return jwt.sign(user, JWT_KEY, { expiresIn: "2d" });
+    } catch (error) {
+      console.log("Something went wrong in token creation");
+      throw error;
+    }
+  }
+  verifyToken(token) {
+    try {
+      return jwt.sign(token, JWT_KEY);
+    } catch (error) {
+      console.log("Something went wrong in token validation");
+      throw error;
+    }
+  }
+  checkPassword(userInputPassword, encryptedPassword) {
+    try {
+      return bcrypt.compareSync(userInputPassword, encryptedPassword);
+    } catch (error) {
+      console.log("Something went wrong in password comparison");
       throw error;
     }
   }
